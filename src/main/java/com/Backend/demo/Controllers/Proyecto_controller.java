f8a01b3cdfc28c2f16d48a9acc4aba1b45b385b1
Mairabel <110679164+Mairabel@users.noreply.github.com>
2023-01-22 12:30:24 -0300

    Update Proyecto_controller.java

--- src/main/java/com/Backend/demo/Controllers/Proyecto_controller.java
+++ src/main/java/com/Backend/demo/Controllers/Proyecto_controller.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/proyecto")
-@CrossOrigin(origins = "https://frontendmyportafoliodepl-45ec8.web.app")
+@CrossOrigin(origins = "http://localhost:8080")
 public class Proyecto_controller{
     @Autowired
     private Proyecto_repository proyecto_repository;
